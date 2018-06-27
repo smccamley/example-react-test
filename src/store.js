98cefb67505c1192402c09a7230a5cfec09e18e4
Stuart McCamley <s.mccamley@gmail.com>
2018-06-28 00:14:32 +0100

    new product added

--- src/store.js
+++ src/store.js
@@ -1,4 +1,5 @@
 export const products = [
-  { name: "Personalised cufflinks", alt: "cufflinks", imageThumbnail: "cufflinks.jpg", price: 45.00, id: 1 },
-  { name: "Kids T-shirt", alt: "kids t-shirt", imageThumbnail: "tshirt.jpg", price: 19.95, id: 2 },
+  { name: "Travel card holder", alt: "Travel card holder", imageThumbnail: "cardholder.jpg", price: 9.25, id: 2 },
+  { name: "Personalised cufflinks", alt: "cufflinks", imageThumbnail: "cufflinks.jpg", price: 45.00, id: 2 },
+  { name: "Kids T-shirt", alt: "kids t-shirt", imageThumbnail: "tshirt.jpg", price: 19.95, id: 3 },
 ]
